58f386ae5d40f749dab19ddc959870c795e145fe
miaohua897 <haijianhoubj@gmail.com>
2024-12-31 14:31:39 -0800

    updated

--- src/mockdata/reviewData.js
+++ src/mockdata/reviewData.js
@@ -1,18 +1,30 @@
 
 export const reviewData=[
     {
+        id:'0',
+        name:'select one...',
+        info:'what do you want to learn today?'
+
+    },
+    {
         id:"1",
-        name:'name1',
-        info:'info1'
+        name:'Cell: The Basic Unit of Life',
+        info:'The cell is the basic structural and functional unit of all living organisms. It is the smallest unit of life capable of performing all the necessary functions for survival, such as metabolism, energy production, and reproduction. Cells can exist as single-celled organisms, like bacteria, or form complex multicellular organisms, such as plants and animals. There are two main types of cells: prokaryotic cells, which lack a nucleus, and eukaryotic cells, which have a defined nucleus and other specialized organelles. The study of cells is fundamental to understanding biology, health, and disease.'
     },
     {
         id:"2",
-        name:'name2',
-        info:'info2'
+        name:'Basic Components of DNA Molecule',
+        info:'DNA molecules are composed of three basic components: sugar (deoxyribose), phosphate groups, and nitrogenous bases. The nitrogenous bases include adenine (A), thymine (T), cytosine (C), and guanine (G), which pair specifically to form the double helix structure of DNA.'
     },
     {
         id:"3",
-        name:'name3',
-        info:'info3'
+        name:'Photosynthesis in Chloroplasts',
+        info:'Photosynthesis in plants primarily occurs in the chloroplasts, which contain chlorophyll. This pigment captures light energy and converts it into chemical energy, enabling the plant to produce glucose and oxygen.'
+    },
+    {
+        id:'4',
+        name:'Gene Expression: The Process of Transcription',
+        info:'Gene expression begins with transcription, where the DNA sequence of a gene is copied into a messenger RNA (mRNA) molecule in the nucleus.'
     }
+
 ]
